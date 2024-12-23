03d2bb84f84e46832b215455c90f7cc6bf9a8f9b
ximaani <mohamedcabdullaahi2@gmail.com>
2024-12-23 16:44:46 +0300

    Version 0.2

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// wWindow Scroll
+
+$(window).on("scroll", function(){
+    var scrollTop = $(window).scrollTop();
+
+    if(scrollTop >= 100){
+        $('body').addClass('fixed-header');
+    } else{
+        $('body').removeClass('fixed-header')
+    }
+});
+
+// Document Ready
+
+$(document).ready(function(){
+    // Typing Animation
+
+    new Typed('#type-it', {
+        strings: ['Designer', 'Developer', 'Freelancer'],
+        typeSpeed: 100,
+        loop: true
+      });
+
+})
